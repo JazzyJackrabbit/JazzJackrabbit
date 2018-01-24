9db625d6301323cfa4895cbf8f1a23f8a13b4d5d
JazzyJackrabbit <imjazzyjackrabbit@gmail.com>
2018-01-24 17:42:44 +0100

    24_01_2018_02

--- Jazz_Jackrabbit_Remake/src/jazz/TestLevel.java
+++ Jazz_Jackrabbit_Remake/src/jazz/TestLevel.java
@@ -6,13 +6,20 @@ import java.io.FileInputStream;
 public class TestLevel {  
 	
      public static void main(String args[]){    
-    	 System.out.print("test");
-          try{    
-            FileInputStream fin=new FileInputStream("test.lvl");    
-            int i=fin.read();  
-            System.out.print((char)i);    
-  
-            fin.close();    
-          }catch(Exception e){System.out.println(e);}    
+    	  try{    
+              FileInputStream fin=new FileInputStream("src\\testout.txt");    
+              
+              int i=0;    
+              while((i=fin.read())!=-1){    
+            	  System.out.print((char)i);    
+              }    
+              
+              i=0;    
+              while((i=fin.read())!=-1){    
+            	  System.out.print(i);    
+              }    
+              
+              fin.close();    
+            }catch(Exception e){System.out.println(e);}    
          }    
         }  
